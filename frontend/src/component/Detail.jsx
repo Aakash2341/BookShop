bfab94cd3abb26560608d971b2069db25d618c31
Aakash2341 <aakashverma2341@gmail.com>
2024-08-09 11:15:35 +0530

    update logo

--- frontend/src/component/Detail.jsx
+++ frontend/src/component/Detail.jsx
@@ -1,79 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-
+import axios from "axios";
+import toast from "react-hot-toast";
 const Detail = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
-    // Handle form submission
-    console.log(data);
-  };
-
   return (
-    <div>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box bg-stone-200 text-black">
-          {/* if there is a button in form, it will close the modal */}
-          <Link
-            to="/"
-            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-            onClick={() => document.getElementById("my_modal_3").close()}
+    <div className="  h-screen flex  bg-white">
+      <div className="w-[1200px]  ">
+        <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+          <div
+            className="w-full  w-1/2 
+       mt-12 md:mt-32 order-1 md:order-1"
           >
-            ✕
-          </Link>
-
-          <h3 className="font-bold text-lg">LogIn</h3>
-
-          <form onSubmit={handleSubmit(onSubmit)} className="mt-4 space-y-4">
-            {/* email */}
-            <div className="space-y-2">
-              <span>Email</span>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="w-80 px-3 py-1 border rounded-md text-black bg-stone-200"
-                {...register("email", { required: "Email is required" })}
-              />
-              {errors.email && (
-                <span className="text-xs text-red-600">
-                  {errors.email.message}
-                </span>
-              )}
-            </div>
-
-            {/* password */}
-            <div className="space-y-2 mt-8">
-              <span>Password</span>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="w-80 px-3 py-1 border rounded-md bg-stone-200 text-black"
-                {...register("password", { required: "Password is required" })}
-              />
-              {errors.password && (
-                <span className="text-xs text-red-600">
-                  {errors.password.message}
-                </span>
-              )}
-            </div>
-
-            {/* button */}
-            <div className="flex justify-around mt-5">
-              <button
-                type="submit"
-                className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200"
+            <div className="  text-black">
+              <Link
+                to="/"
+                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                onClick={() => document.getElementById("my_modal_3").closed()}
               >
-                Login
-              </button>
+                ✕
+              </Link>
+
+              <h3 className="font-bold text-lg">Contact Detail</h3>
+
+              <div className=" mt-4 space-y-2">
+                <span>FuLL Name</span>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Enter your Name"
+                  className="w-80 px-3 py-1 border rounded-md  text-black bg-stone-200"
+                />
+                <br />
+              </div>
+
+              <div className="  space-y-2 mt-8">
+                <span>Phone No.</span>
+                <br />
+                <input
+                  type="digit"
+                  placeholder="Enter your Number"
+                  className="w-80 px-3 py-1 border rounded-md  bg-stone-200  text-black
+              "
+                />
+                <br />
+              </div>
+              <div className="  space-y-2 mt-8">
+                <span>Address</span>
+                <br />
+
+                <input
+                  type="digit"
+                  placeholder="Enter your Number"
+                  className="w-80 px-3 py-1 border rounded-md  bg-stone-200  text-black
+              "
+                />
+                <br />
+              </div>
+
+              <div className=" mt-3">
+                <button className=" bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                  Login
+                </button>
+              </div>
             </div>
-          </form>
+          </div>
         </div>
-      </dialog>
+        <div className="  w-20  md:mt-24 ml-0 md: ml- 4 mb-4 mt-24">
+          <img src="/banner.jpg" className=" w-120 h-105 " alt="" />
+        </div>
+      </div>
     </div>
   );
 };
